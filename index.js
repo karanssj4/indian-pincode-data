386b61ab05665d1d198cc866e0c9c8482cce5c92
Karan Raina <karan95raina@gmail.com>
2017-08-04 13:22:00 +0530

    v1
    Signed-off-by: Karan Raina <karan95raina@gmail.com>

---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csvtojson');
+
+// const data =  fs.readFileSync(path.join(__dirname, 'res/test.csv'), { encoding : 'utf8'});
+const data =  fs.readFileSync(path.join(__dirname, 'res/all_india_pin_code.csv'), { encoding : 'utf8'});
+const result = [];
+
+
+
+
+csv({
+    colParser:{
+      "officeType": "omit",
+      "Deliverystatus": "omit",
+      "officename":function(item, head, resultRow, row , colIdx){
+            /*
+                item - "1970-01-01"
+                head - "birthday"
+                resultRow - {name:"Joe"}
+                row - ["Joe","1970-01-01"]
+                colIdx - 1
+            */
+            // return new Date(item);
+            return item.replace(/ (B|S|H).O/g, "")
+        }
+    }
+}).fromString(data)
+.on('json', (json) => {
+  result.push(json);
+})
+.on('done', () => {
+  fs.writeFileSync('res/pincodeData.min.json', JSON.stringify(result));
+  fs.writeFileSync('res/pincodeData.pretty.json', JSON.stringify(result, null, 1));
+  console.log('end');
+})
